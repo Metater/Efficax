f01f35fee6cecf8d1e56057609e87c781247f2c0
Metater <61102529+Metater@users.noreply.github.com>
2020-08-15 16:49:44 -0400

    Delete Commands.java
    Was renamed to CommandHandler

--- src/main/java/com/github/metater/efficax/commands/Commands.java
+++
@@ -1,9 +0,0 @@
-package com.github.metater.efficax.commands;
-
-import com.github.metater.efficax.Efficax;
-
-public class Commands {
-    public void Init(Efficax efficax) {
-        efficax.getCommand("diamonds").setExecutor(new CommandDiamonds());
-    }
-}
